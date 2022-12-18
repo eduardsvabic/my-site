f55baf0100d8102229d7887d1d2a75da08f008ec
eduardsvabic <eduardsvabic@gmail.com>
2022-12-18 08:46:02 +0200

    bruh

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function(){
+Route::get('/', 'App\Http\Controllers\TaskController@index');
+Route::resource('tasks', 'App\Http\Controllers\TaskController');
+});
