4266f476ff44dc536f552430c904c2b70360f08c
eduardsvabic <eduardsvabic@gmail.com>
2022-12-18 09:48:33 +0200

    fixed url bug

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,15 +6,12 @@
  @foreach($products as $product)
  <div class="col-xs-18 col-sm-6 col-md-3">
  <div class="thumbnail">
- <img src="{{ $product->photo }}" width="500"
-height="300">
+ <img src="{{ $product->photo }}" width="500" height="300">
  <div class="caption">
  <h4>{{ $product->name }}</h4>
- <p>{{ str_limit(strtolower($product->description), 50)
-}}</p>
- <p><strong>Pret: </strong> {{ $product->price
-}}$</p>
- <p class="btn-holder"><a href="{{ url('add-tocart/'.$product->id) }}" class="btn btn-warning btn-block text-center"
+ <p>{{ str_limit(strtolower($product->description), 50)}}</p>
+ <p><strong>Pret: </strong> {{ $product->price }}$</p>
+ <p class="btn-holder"><a href="{{ url('add-to-cart/'.$product->id) }}" class="btn btn-warning btn-block text-center"
 role="button">Pune in cos</a> </p>
  </div>
  </div>
